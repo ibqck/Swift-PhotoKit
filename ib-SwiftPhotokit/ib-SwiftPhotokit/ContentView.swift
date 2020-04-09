e1ae8ae6c44b3240595ff7c24309bb820947e7f2
ibqck <nankkang12@naver.com>
2020-04-09 10:19:27 +0900

    Add files via upload
    Create Project

---
+++ ib-SwiftPhotokit/ib-SwiftPhotokit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ib-SwiftPhotokit
+//
+//  Created by INBEOM on 2020/04/09.
+//  Copyright © 2020 ibp. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
